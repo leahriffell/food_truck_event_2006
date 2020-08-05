475c62a8392998e4e0704023e7b4256084c491e7
Leah Riffell <leah.riffell@gmail.com>
2020-08-05 09:13:31 -0600

    Add Event initial test

---
+++ test/event_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+
+class EventTest <  Minitest::Test
+  def setup 
+    @event = Event.new("South Pearl Street Farmers Market")    
+  end
+
+  def test_it_exists
+    assert_instance_of Event, @event
+  end
+end
