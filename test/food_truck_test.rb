4d66e03b1dab06bb6c203eef8d682ca12b9e82e1
Leah Riffell <leah.riffell@gmail.com>
2020-08-05 09:07:50 -0600

    Add test for adding inventory to FoodTruck and checking stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -12,4 +12,18 @@ class FoodTruckTest <  Minitest::Test
     assert_instance_of FoodTruck, @food_truck
     assert_equal "Rocky Mountain Pies", @food_truck.name
   end
+
+  def test_it_can_add_inventory_and_check_stock 
+    assert_equal ({}), @food_truck.inventory 
+    assert_equal 0, @food_truck.check_stock(@item1)
+    
+    @food_truck.stock(@item1, 30)
+    assert_equal 30, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item1, 25)
+    assert_equal 55, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item2, 12)
+    assert_equal ({@item1 => 55, @item2 => 12}), @food_truck.inventory 
+  end
 end
