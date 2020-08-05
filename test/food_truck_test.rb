79db6af03f85f54b12b823dcd66496336b132adc
Leah Riffell <leah.riffell@gmail.com>
2020-08-05 09:34:53 -0600

    Add more FoodTruck instances to FoodTruck test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -6,8 +6,13 @@ require './lib/food_truck'
 class FoodTruckTest <  Minitest::Test
   def setup 
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")    
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")  
+
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   def test_it_exists_and_has_readable_attributes
@@ -30,7 +35,13 @@ class FoodTruckTest <  Minitest::Test
   end
 
   def test_it_can_calculate_its_potential_revenue 
-    assert_equal 148.75, @food_truck1.potential_revenue
+    @food_truck.stock(@item1, 35)    
+    @food_truck.stock(@item2, 7)    
+    @food_truck2.stock(@item4, 50)    
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)  
+
+    assert_equal 148.75, @food_truck.potential_revenue
     assert_equal 345.00, @food_truck2.potential_revenue
     assert_equal 243.75, @food_truck3.potential_revenue
   end
