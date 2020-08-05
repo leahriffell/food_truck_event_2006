ac99179c9af40219ef02af746e924b6de5bbeec9
Leah Riffell <leah.riffell@gmail.com>
2020-08-05 09:00:19 -0600

    Add tests for readable attributes for Item

--- test/item_test.rb
+++ test/item_test.rb
@@ -8,7 +8,9 @@ class ItemTest <  Minitest::Test
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
-  def test_it_exists
+  def test_it_exists_and_has_readable_attributes
     assert_instance_of Item, @item1
+    assert_equal "Apple Pie (Slice)", @item2.name 
+    assert_equal 2.50, @item.price
   end
 end
