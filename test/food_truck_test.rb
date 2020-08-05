6ea324debb6a54dc3e6b8eec70950683aad4c6c0
Leah Riffell <leah.riffell@gmail.com>
2020-08-05 09:04:02 -0600

    Add readable attribute tests for FoodTruck

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -8,7 +8,8 @@ class FoodTruckTest <  Minitest::Test
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
   end
 
-  def test_it_exists
+  def test_it_exists_and_has_readable_attributes
     assert_instance_of FoodTruck, @food_truck
+    assert_equal "Rocky Mountain Pies", @food_truck.name
   end
 end
