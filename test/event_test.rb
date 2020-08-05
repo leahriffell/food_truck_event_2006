b3a1fc153e9f828517108472a921ccca2be2150e
Leah Riffell <leah.riffell@gmail.com>
2020-08-05 09:23:36 -0600

    Add test for returning all FoodTruck names

--- test/event_test.rb
+++ test/event_test.rb
@@ -43,4 +43,9 @@ class EventTest <  Minitest::Test
     event2.add_food_truck(@food_truck3)
     assert_equal [@food_truck1, @food_truck2, @food_truck3], event2.food_trucks
   end
+
+  def test_it_can_return_names_of_all_food_trucks 
+    assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @event.food_truck_names
+
+  end
 end
